087034a08f6c8bf6f5c722c5ed73ac3f274909b7
bverjat <benoit.verjat@sciences-po.fr>
2015-07-03 16:49:57 +0200

    working on D3 version and data format

--- app.js
+++ app.js
@@ -15,11 +15,17 @@ var tasks = {};
 
 dbLocal.query("MATCH (m) WITH labels(m) AS l return DISTINCT l", function(err, result) {
   if (err) throw err;
-
-  labels = result;
-  getKeys(labels);
-
+  getKeys(result);
 });
+
+var getIndexIfObjWithAttr = function(array, attr, value) {
+  for(var i = 0; i < array.length; i++) {
+      if(array[i][attr] === value) {
+          return i;
+      }
+  }
+  return -1;
+}
 
 function labelsToQuery(labels){
 
@@ -32,32 +38,24 @@ function labelsToQuery(labels){
 
   return query;
 }
-
-function genDotLabel(labels){
-  return _(labels).toString().replace(',','_');
-}
-
 function getKeys(labels){
 
   _.forEach(labels, function(label) {
-
-
-      // console.log("q", "MATCH m WHERE "+labelsToQuery(label)+" RETURN DISTINCT keys(m)")
 
       tasks[label] =  function(next) {
 
         async.parallel(
           {
-            props: function(next) {
+            props: function(next2) {
               dbLocal.query("MATCH m WHERE "+labelsToQuery(label)+" RETURN DISTINCT keys(m)", function(err, result) {
                 if (err) throw err;
-                return next(null, _.union(_.flatten(result)));
+                return next2(null, _.union(_.flatten(result)));
               });
             },
-            relations: function(next){
+            relations: function(next2){
               dbLocal.query("MATCH (m)-[r]->(t) WHERE "+labelsToQuery(label)+" RETURN DISTINCT type(r) as type ,labels(t) as labels", function(err, result) {
                 if (err) throw err;
-                return next(null, _.flatten(result));
+                return next2(null, _.flatten(result));
               });
             }
           },
@@ -72,15 +70,48 @@ function getKeys(labels){
 
   async.parallel(tasks,
     function(err, results) {
-      fs.writeFile("data.json", JSON.stringify(results), function(err) {
-        if(err) return console.log(err);
-        console.log("Data saved!");
-      });
-
        genDotGraph(results);
-
+       saveJSON(results);
     }
   );
+}
+function saveJSON(results){
+
+  var data = {nodes:[], edges:[]};
+  var n =0;
+
+  fs.writeFileSync("data/raw_data.json", JSON.stringify(results));
+  console.log("rawdata saved!");
+
+  _.forEach(results, function(node, key) {
+
+    // create Node (unique label combination )
+    var newNode = {type:'entity', label:key, name:key, id:data.nodes.length}
+    data.nodes.push(newNode);
+
+    // _.forEach(node.props, function(prop) {
+
+    //   // create Node Property keys
+    //   var newProp = {type:'key', label:prop, name:prop, id:data.nodes.length};
+    //   data.nodes.push(newProp);
+
+    //   // link Property keys to label
+    //   data.edges.push({type:'key', source:newNode.id, target:newProp.id, label:''});
+    // });
+  });
+  _.forEach(results, function(node, key) {
+    _.forEach(node.relations, function(relation) {
+
+      sourceindex = getIndexIfObjWithAttr(data.nodes, "name", key);
+      targetindex = getIndexIfObjWithAttr(data.nodes, "name", _(relation.labels).toString());
+
+      data.edges.push({type:'predicate', source:sourceindex, target:targetindex, name:relation.type});
+
+    });
+  });
+
+  fs.writeFileSync("data/data.json", JSON.stringify(data));
+  console.log("data saved!");
 }
 
 function genDotGraph(results){
@@ -98,23 +129,24 @@ function genDotGraph(results){
 
       keys +=  " "+name+"[label= "+prop+"]";
       keylinks = keylinks +" "+key+"->"+name + "[dir=none, style=dashed]";
+
     });
 
     _.forEach(node.relations, function(relation) {
-        links += " "+key+"->"+genDotLabel(relation.labels)+"[label=  "+relation.type+"]";
+        links += " "+key+"->"+_(relation.labels).toString().replace(',','_')+"[label=  "+relation.type+"]";
     });
+
   });
 
   nodes = 'subgraph { node [shape=hexagon style=filled, fillcolor=black, color=white fontcolor=white]; edge [penwidth=100]; '+ nodes +'}';
   keys  = 'subgraph { node [shape=invhouse]; '+ keys +'}';
-  settings = ' ranksep=3; layout=dot; '
-
+  settings = ' ranksep=3; layout=dot; ';
 
   graph = 'digraph  {'+ settings +' '+nodes+' '+keys+' '+keylinks+' '+ links +'}';
-  graphlight = 'digraph  { '+ settings +' '+nodes+' '+ links +'}';
-
-  fs.writeFileSync("graph.dot", graph);
+  fs.writeFileSync("data/graph.dot", graph);
   console.log("graph saved!");
-  fs.writeFileSync("graphlight.dot", graphlight);
+
+  graphlight = 'digraph  { '+ settings +' '+nodes+' '+ links +'}';
+  fs.writeFileSync("data/graphlight.dot", graphlight);
   console.log("graphlight saved!");
 }
